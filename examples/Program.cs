4c6490c08d4fbc33c056eb8a27e82165a2cc570f
Renata19 <renatalsa@mail.ru>
2022-09-12 21:04:13 +0700

    example

---
+++ examples/Program.cs
@@ -0,0 +1,36 @@
+﻿// “— Я думаю, — сказал князь, улыбаясь, — что,”
+//  ”ежели бы вас послали вместо нашего милого Винценгероде,”
+//  ”вы бы взяли приступом согласие прусского короля.”
+//  ”Вы так красноречивы. Вы дадите мне чаю?”;
+
+string text ="— Я думаю, — сказал князь, улыбаясь, — что,"
+ + "ежели бы вас послали вместо нашего милого Винценгероде,"
+ + "вы бы взяли приступом согласие прусского короля, "
+ + "Вы так красноречивы. Вы дадите мне чаю?";
+
+//  string s = "qwerty"
+//              123
+//  s[3] r
+
+string Replase(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+int length = text.Length;
+for(int i = 0; i < length; i++)
+{
+    if(text[i] == oldValue) result = result + $"{newValue}";
+    else result = result + $"{text[i]}";
+    
+}
+
+    return result;
+}
+
+string newText = Replase(text, ' ', '|');
+Console.WriteLine();
+newText = Replase(text, 'к', 'К');
+Console.WriteLine();
+newText = Replase(text, 'С', 'с');
+
+Console.WriteLine(newText);
