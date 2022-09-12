101f6107e48a69870e877291095ca75dd8c41968
Renata19 <renatalsa@mail.ru>
2022-09-12 21:22:53 +0700

    massive

---
+++ massive/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+Console.WriteLine();
+} 
+
+void SelectionSort(int[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int minPisition = i;
+
+        for(int j = i + 1; j < array.Length; j++)
+    {
+        if(array[j] < array[minPisition]) minPisition = j;
+    }
+
+        int temporary = array[i];
+        array[i] = array[minPisition];
+        array[minPisition] = temporary;
+    }
+}
+
+PrintArray(arr);
+
+SelectionSort(arr);
+
+PrintArray(arr);
+
